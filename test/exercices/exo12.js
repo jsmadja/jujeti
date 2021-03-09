729f17ad09ed3296064141e501b17ab7973d40c4
jeje59223 <jcnockaert@hotmail.fr>
2021-03-09 12:13:39 +0100

    exercice 12 completed

---
+++ test/exercices/exo12.js
@@ -0,0 +1,18 @@
+// Ordre décroissant
+// Votre tâche consiste à créer une fonction qui peut prendre n'importe quel entier non négatif
+// comme argument et le renvoyer avec ses chiffres dans l'ordre décroissant. Essentiellement,
+// réorganisez les chiffres pour créer le nombre le plus élevé possible.
+//
+// Exemples:
+// Entrée: 42145 Sortie:54421
+
+function descendingOrder(n) {
+  const tab = n.toString().split('')
+    .map((nb) => nb)
+    .sort((a, b) => b - a)
+    .join('');
+  // eslint-disable-next-line radix
+  return parseInt(tab);
+}
+
+console.log(descendingOrder(6547189));
