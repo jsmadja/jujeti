ee3df57f3344179ab8020434d9d48e4159f6d5ae
jeje59223 <jcnockaert@hotmail.fr>
2021-03-10 12:16:39 +0100

    ajout des tests sur exo 6 et 7

---
+++ src/exo6.spec.ts
@@ -0,0 +1,15 @@
+const convertiseur = (degCelsius: number) => {
+  const conversion = degCelsius * (9 / 5) + 32;
+  if (degCelsius) {
+    return conversion;
+  }
+  return 'Données en entrée non correctes.';
+};
+
+test(`Créer un algorithme qui sert à convertir des degrés Celsius
+en degrés Fahrenheit.
+Le résultat doit être de type nombre.
+Si on donne un autre type qu'un nombre en argument, vous
+devez retourner "Données en entrée non correctes."`, () => {
+  expect(convertiseur(1)).toBe(33.8);
+});
